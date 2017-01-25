1140875e4efed6474546ee3f53312af94c532d74
Justin Haar <justinhaar@Justins-MacBook-Pro.local>
2017-01-24 19:29:56 -0500

    Initial Commit

---
+++ Media Player/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Media Player
+//
+//  Created by Justin Haar on 1/24/17.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Media Player/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Media Player
+//
+//  Created by Justin Haar on 1/24/17.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
